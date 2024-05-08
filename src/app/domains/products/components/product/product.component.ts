3c05402c34280c4e976a9f75fb616d81320beade
Alexander Jacho <alexjacho77@gmail.com>
2024-05-08 14:55:00 +0300

    The process of setting up information transfer from the child to the father component has begun. Within the child component (product.ts), I've created my own @Output named addToCart, which is a decorator, alongside EventEmitter (a stay). Additionally, the addCartHandler event has been defined to emit a message from the child through the addToCart output, utilizing the emit method.

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-product',
@@ -11,5 +11,12 @@ export class ProductComponent {
   @Input({required:true}) img: string = '';
   @Input({required:true}) price: number = 0;
   @Input({required:true}) title: string = '';
+
+  @Output () addToCart = new EventEmitter();
+
+  addCartHandler(){
+    console.log('click from child');
+    this.addToCart.emit('its a message from child');
+  }
 
 }
