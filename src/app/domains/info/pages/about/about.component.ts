23f0940e08aee73acaa0052e575847a35dac6b56
Alexander Jacho <alexjacho77@gmail.com>
2024-05-10 17:12:24 +0300

    About.component is set up to use counter.component through import

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CounterComponent} from './../../../shared/components/counter/counter.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [CounterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  
 
 }
