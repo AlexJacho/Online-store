240cc7ffa39e4b9dea575efc7fd26caaabc5ad74
Alexander Jacho <alexjacho77@gmail.com>
2024-05-11 17:04:02 +0300

    In about.ts, two signals were created (duration and message), and events were established to dynamically change these signals (changeDuration and changeMessage), which will be read as inputs.

--- src/app/domains/info/pages/about/about.component.ts
+++ src/app/domains/info/pages/about/about.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CounterComponent} from './../../../shared/components/counter/counter.component';
 
 @Component({
@@ -9,6 +9,18 @@ import { CounterComponent} from './../../../shared/components/counter/counter.co
   styleUrl: './about.component.css'
 })
 export class AboutComponent {
+  duration = signal(1000);
+  message = signal('hola');
   
+  changeDuration(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.duration.set(input.valueAsNumber)
+  }  
+  changeMessage(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.message.set(input.value)
+  }
+  
+
 
 }
