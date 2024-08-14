c031756de368ef0226e46330e09af13f1e19d3a6
Alexander Jacho <alexjacho77@gmail.com>
2024-08-14 12:06:51 +0300

    routerLink – Linking pages
    Objective:
    Enable seamless navigation between the Home, About, Services and Pricing pages Link the about, service and home page using Angular’s routerLink for Single[Page-Application (SPA) functionality.
    
    Solution:
    To link the pages, I replaced the Href tags with routerLink attribute in the header layout.
    
    In header.ts:
    Imported the RouterLinkWithHref from the Angular’s router package to enable internal routing.
    
    In header.html:
    Replaced the href tags with the routerLink for the home, about and service pages. This allows the application to link these pages efficiently without reloading the entire page.

--- src/app/domains/shared/components/header/header.component.ts
+++ src/app/domains/shared/components/header/header.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, Input, signal, SimpleChanges } from '@angular/core';
 import { Product } from '../../models/product.model';
 import { CommonModule } from '@angular/common';
 import { CartsideService } from '@shared/services/cartside.service';
+import { RouterLinkWithHref } from '@angular/router';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLinkWithHref],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
